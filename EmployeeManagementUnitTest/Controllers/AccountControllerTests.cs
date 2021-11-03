b36e48fd3e6ab6bad852cea21f1e91400d29ad9d
@d.argastcev <daniil.argastsev@solbeg.com>
2021-11-03 10:47:29 +0300

    Update files and add UnitTests

---
+++ EmployeeManagementUnitTest/Controllers/AccountControllerTests.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using EmployeeManagement.Controllers;
+using EmployeeManagement.Models.Service.Interface;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementUnitTest.Controllers
+{
+    [TestClass]
+    public class AccountControllerTests
+    {
+        private MockRepository mockRepository;
+
+        private Mock<IMapper> mockMapper;
+        private Mock<IUserService> mockUserService;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.mockRepository = new MockRepository(MockBehavior.Strict);
+
+            this.mockMapper = this.mockRepository.Create<IMapper>();
+            this.mockUserService = this.mockRepository.Create<IUserService>();
+        }
+
+        private AccountController CreateAccountController()
+        {
+            return new AccountController(
+                this.mockMapper.Object,
+                this.mockUserService.Object);
+        }
+
+        [TestMethod]
+        public void Login_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var accountController = this.CreateAccountController();
+
+            // Act
+            var result = accountController.Login();
+
+            // Assert
+            Assert.Fail();
+            this.mockRepository.VerifyAll();
+        }
+
+        [TestMethod]
+        public async Task Login_StateUnderTest_ExpectedBehavior1()
+        {
+            // Arrange
+            var accountController = this.CreateAccountController();
+            UserViewModel model = null;
+
+            // Act
+            var result = await accountController.Login(
+                model);
+
+            // Assert
+            Assert.Fail();
+            this.mockRepository.VerifyAll();
+        }
+    }
+}
---
+++ EmployeeManagementUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmployeeManagementUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EmployeeManagementUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ded6cd58-9a42-43f8-a293-93b35b49e315")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
